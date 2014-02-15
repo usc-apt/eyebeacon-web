13e474c716b6b77c4feb50b32b7a4f4f378c3660
Vincente Ciancio <vciancio@socaldevs.com>
2014-02-15 15:44:05 -0500

    Wrote MirrorClient the right way.

--- src/com/suchbeacon/web/MirrorClient.java
+++ src/com/suchbeacon/web/MirrorClient.java
@@ -9,6 +9,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 import com.suchbeacon.web.Card.ActionItem;
 
 public class MirrorClient {
@@ -16,20 +19,21 @@ public class MirrorClient {
 		try {
 			URL url = new URL("https://www.googleapis.com/mirror/v1/timeline");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			JSONObject jsonBody = new JSONObject();
 			connection.setDoOutput(true);
 			connection.setRequestMethod("POST");
 			connection.setRequestProperty("Authorization", "Bearer " + authToken);
 			connection.setRequestProperty("Content-Type", "application/json");
 
 			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-			String s = "{ html: '" + card.getHtml() + "', "
-					+ generateBundleCover(card.isBundleCover()) + ", "
-					+ generateBundleId(card.getBundleId()) + ", "
-					+ generateActionItems(card.getActionItems()) + ", "
-					+ generateNotification() + ", "
-					+ generateSpeakableText(card.getSpeakableText()) + " }";
-			System.out.println(s);
-			writer.write(s);
+			jsonBody.put("html", card.getHtml());
+			jsonBody.put("isBundleCover", card.isBundleCover());
+			jsonBody.put("bundleId", card.getBundleId());
+			jsonBody.put("menuItems", generateActionItems(card.getActionItems()));
+			jsonBody.put("notification", generateNotification());
+			jsonBody.put("speakableText", card.getSpeakableText());
+			System.out.println(jsonBody.toString());
+			writer.write(jsonBody.toString());
 			writer.close();
 
 			if (connection.getResponseCode() == HttpURLConnection.HTTP_CREATED
@@ -50,53 +54,46 @@ public class MirrorClient {
 			System.out.println("IO");
 			System.out.println(e.getMessage());
 			return new GlassResponse("400 Bad Request", "IO Exception");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return new GlassResponse("400 Bad Request", "JSON Exception");
 		}
 	}
-	
-	private static String generateBundleCover(boolean isBundleCover) {
-		return "isBundleCover: " + isBundleCover;
-	}
 
-	private static String generateBundleId(String bundleId) {
-		return "bundleId: '" + bundleId + "'";
-	}
-
-	private static String generateActionItems(List<ActionItem> actionItems) {
-		String ai = "menuItems: [ ";
+	private static JSONArray generateActionItems(List<ActionItem> actionItems) throws JSONException{
+		
+		JSONArray menuItems = new JSONArray();
+		
 		for (ActionItem a : actionItems) {
-			ai += "{";
+			JSONObject menuItem = new JSONObject();
+			menuItem.put("action", a.action);
 			if (!a.action.equals("CUSTOM")) {
-				ai += "action: '" + a.action + "'";
 				if (a.payload != null) {
-					ai += ",";
-					ai += "payload: '" + a.payload + "'";
+					menuItem.put("payload",  a.payload);
 				}
 			} else {
 				// Custom so we need to set that up.
-				ai += "id: '" + a.id + "', ";
-				ai += "action: '" + a.action + "', ";
-				ai += "values: [";
-				ai += "{";
-				ai += "state: '" + a.state + "', ";
-				ai += "displayName: '" + a.displayName + "'";
-				if (a.iconUrl != null) ai += ", iconUrl: '" + a.iconUrl + "'";
+				menuItem.put("id", a.id);
+				
+				JSONArray valuesArray = new JSONArray();
+				
+				JSONObject value = new JSONObject();
+				value.put("state", a.state);
+				value.put("displayName", a.displayName);
+				if (a.iconUrl != null) value.put("iconUrl", a.iconUrl);
+				
+				valuesArray.put(value);
+				menuItem.put("values", valuesArray);
 			}
-			ai += "}";
-			ai += ", ";
+			menuItems.put(menuItem);
 		}
-		ai = ai.substring(0, ai.length() - 2);
-		ai += " ] ";
-		return ai;
+		return menuItems;
 	}
 	
-	private static String generateNotification() {
-		return "notification: { level: 'DEFAULT' }";
-	}
-
-	private static String generateSpeakableText(String speakableText) {
-		String st = "speakableText: '";
-		st += speakableText;
-		st += "'";
-		return st;
+	private static JSONObject generateNotification() throws JSONException{
+		JSONObject notification = new JSONObject();
+		notification.put("level",  "DEFAULT");
+		return notification;
 	}
 }
